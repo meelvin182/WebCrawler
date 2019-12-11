cc75f4332b07d58a73c8cebdc39fbc38b7bcf448
meelvin182 <December0912>
2019-12-11 17:13:46 +0300

    refactored tests + async client cleanup

--- src/test/java/com/scalablecapital/BasicWebTest.java
+++ src/test/java/com/scalablecapital/BasicWebTest.java
@@ -3,59 +3,38 @@ package com.scalablecapital;
 import org.junit.Test;
 import org.mockito.Mockito;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.http.HttpClient;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.*;
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.LongAdder;
-import java.util.concurrent.locks.LockSupport;
-import java.util.stream.Collectors;
 
 
 public class BasicWebTest {
 
     ConcurrentHashMap<String, LongAdder> storage = new ConcurrentHashMap<>();
 
-    protected PageDownloader pageDownloader = Mockito.mock(PageDownloader.class);
+    protected HttpClient httpClientMock = Mockito.mock(HttpClient.class);
+    protected PageDownloader pageDownloaderMock = Mockito.mock(PageDownloader.class);
 
-    protected GoogleSearcher googleSearcher = Mockito.mock(GoogleSearcher.class);
+    protected GoogleSearcher googleSearcherMock = Mockito.mock(GoogleSearcher.class);
 
-    protected String getFileFromResourceFolder(String path) throws URISyntaxException, IOException {
-        return new String(Files.readAllBytes(Paths.get(ClassLoader.getSystemResource(path).toURI())));
+    protected String getFileFromResourceFolder(String path) throws URISyntaxException {
+
+        try {
+            return new String(Files.readAllBytes(Paths.get(ClassLoader.getSystemResource(path).toURI())));
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("file with name "+path+" not found");
+        }
+        return null;
     }
-
 
     @Test
-    public void name() throws ExecutionException, InterruptedException
-    {
-        List<String> results = CompletableFuture.supplyAsync(() -> {
-            System.out.println("main");
-            return "main + ";
-        }).thenCompose(mainResult -> {
-            List<CompletableFuture<String>> futures = new ArrayList<>();
-            futures.add(CompletableFuture.supplyAsync(() -> {
-                LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(1));
-                System.out.println("finish foo");
-                return mainResult + "foo";
-            }));
-            futures.add(CompletableFuture.supplyAsync(() -> {
-                LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(3));
-                System.out.println("bar");
-                return mainResult + "bar";
-            }));
+    public void name() {
+        System.out.println(pageDownloaderMock.downloadPages(Collections.singletonList("google.com")));
 
-            return CompletableFuture.allOf(futures.toArray(new CompletableFuture[2]))
-                    .thenApply(ignore -> {
-                        return futures.stream().map(CompletableFuture::join).collect(Collectors.toList());
-                    });
-        }).get();
-
-        System.out.println(results);
     }
-
-
 }
