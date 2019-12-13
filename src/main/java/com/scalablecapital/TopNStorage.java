0312b90789b003dc8654235c142dc55c0ae51458
meelvin182 <sokolov@phystech.edu>
2019-12-13 11:18:43 +0300

    fix comparator

---
+++ src/main/java/com/scalablecapital/TopNStorage.java
@@ -0,0 +1,38 @@
+package com.scalablecapital;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TopNStorage<T extends Comparable<T>> {
+
+    private final int n;
+    private final Map<T, Integer> store = new LinkedHashMap<>();
+
+    public TopNStorage(int n) {
+        this.n = n;
+    }
+
+    public synchronized void add(T element) {
+        store.compute(element, (elem, count) -> {
+            if (count == null) {
+                return 1;
+            } else {
+                return count + 1;
+            }
+        });
+    }
+
+    public synchronized List<String> getTop() {
+        List<String> sorted = store.entrySet().stream()
+                .sorted(Comparator.comparingInt(Map.Entry::getValue))
+                .map(e -> "js='" + e.getKey() + "\t=\t" + e.getValue() + "\n")
+                .collect(Collectors.toList());
+        Collections.reverse(sorted);
+        return sorted.stream().limit(5).collect(Collectors.toList());
+    }
+
+    public synchronized Map<T, Integer> getStore() {
+        return store;
+    }
+
+}
